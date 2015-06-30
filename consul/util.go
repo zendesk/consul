3c9873c26cd3d5f17b4f704b24e8c35c0db7d833
William Tisäter <william@defunct.cc>
2015-06-30 18:12:17 +0200

    Treat 169.254.0.0/16 as private network

--- consul/util.go
+++ consul/util.go
@@ -41,7 +41,7 @@ func (s *serverParts) String() string {
 
 func init() {
 	// Add each private block
-	privateBlocks = make([]*net.IPNet, 4)
+	privateBlocks = make([]*net.IPNet, 5)
 	_, block, err := net.ParseCIDR("10.0.0.0/8")
 	if err != nil {
 		panic(fmt.Sprintf("Bad cidr. Got %v", err))
@@ -65,6 +65,12 @@ func init() {
 		panic(fmt.Sprintf("Bad cidr. Got %v", err))
 	}
 	privateBlocks[3] = block
+
+	_, block, err = net.ParseCIDR("169.254.0.0/16")
+	if err != nil {
+		panic(fmt.Sprintf("Bad cidr. Got %v", err))
+	}
+	privateBlocks[4] = block
 }
 
 // strContains checks if a list contains a string
