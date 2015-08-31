4e53804bbbe878a4a152291ce5a2166a334534a9
Michael S. Fischer <mfischer@zendesk.com>
2015-08-31 16:33:09 -0700

    lock.go: fix another race condition

--- command/lock.go
+++ command/lock.go
@@ -168,7 +168,10 @@ func (c *LockCommand) Run(args []string) int {
 		goto RELEASE
 	}
 
-	// Kill the child
+	// Prevent starting a new child.  The lock is never released
+	// after this point.
+	c.childLock.Lock()
+	// Kill any existing child
 	if err := c.killChild(childDone); err != nil {
 		c.Ui.Error(fmt.Sprintf("%s", err))
 	}
@@ -294,9 +297,7 @@ func (c *LockCommand) startChild(script string, doneCh chan struct{}) error {
 // on the first attempt.
 func (c *LockCommand) killChild(childDone chan struct{}) error {
 	// Get the child process
-	c.childLock.Lock()
 	child := c.child
-	c.childLock.Unlock()
 
 	// If there is no child process (failed to start), we can quit early
 	if child == nil {
